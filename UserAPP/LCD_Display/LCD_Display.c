58d6a895eff90f975b9d787f95c7b37d0cf43139
MengBowen <1521677378@qq.com>
2023-08-23 08:08:25 +0800

    Initial commit

---
+++ UserAPP/LCD_Display/LCD_Display.c
@@ -0,0 +1,11 @@
+//
+// Created by 15216 on 2023/8/23.
+//
+#include "LCD_Display.h"
+
+
+
+void LCD_Display(void)
+{
+    ILI9341_DispStringLine_EN(LINE(0), "Smart vegetable planter");
+}
---
+++ UserAPP/LCD_Display/LCD_Display.h
@@ -0,0 +1,13 @@
+//
+// Created by 15216 on 2023/8/23.
+//
+
+#ifndef FIRMWARE_LCD_DISPLAY_H
+#define FIRMWARE_LCD_DISPLAY_H
+
+#include "main.h"
+#include "font.h"
+#include "LCD.h"
+
+void LCD_Display(void);
+#endif //FIRMWARE_LCD_DISPLAY_H
